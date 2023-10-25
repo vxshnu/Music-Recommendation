e6961579f734f485c5e4a1d68f6ab6b6b7f2addc
Vishnu Narayanan <104455288+vxshnu@users.noreply.github.com>
2023-10-25 23:21:46 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,24 +1,26 @@
-
 import spotipy
-from flask import Flask,render_template,request
+import mysql.connector  
+from flask import Flask,render_template,request,url_for,redirect
+
+myconn = mysql.connector.connect(host = "127.0.0.1", user = "root",passwd = "root")  
+db=myconn.cursor()
+
 
 sp = spotipy.Spotify(auth_manager=spotipy.oauth2.SpotifyOAuth(client_id='a24c797e8e9a487a8c6f9aaceadd3eab', client_secret='87183cc80b114379b637810a397c3f60', redirect_uri='http://127.0.0.1:5000/'))
-
 access_token = sp.auth_manager.get_access_token()['access_token']
 
 app=Flask(__name__)
 
+user_active=""
+email_id=""
+
 @app.route('/')
 def home():
-   return render_template("firstpg.html")
-
-@app.route('/login')
-def login():
-   return render_template("LogInPg.html")
-
-@app.route('/signup')
-def signup():
-   return render_template("signup.html")
+   if user_active:
+       return render_template("firstpg.html",n=user_active)
+   else:
+       return render_template("firstpg.html",n="LOGIN")
+   
 
 @app.route('/artist')
 def artist(name):
@@ -28,17 +30,19 @@ def artist(name):
         if find_artist['artists']['items']:
             artist_uri = find_artist['artists']['items'][0]['uri']
         else:
-            return render_template("error.html")
-
+            return render_template("error.html",message="Artist not found")
         if artist_uri:
             top_tracks = sp.artist_top_tracks(artist_uri)
             artist_tracks = top_tracks['tracks']
             embed_links = [f"{track['id']}" for track in artist_tracks]
+            # embed_links.pop(8)
+            # embed_links.pop(8)
             print(f"Embed link: {embed_links}")
-
-            return render_template("rec.html",recommendation=top_tracks,urls=embed_links,names=artist_name)
+            if user_active:
+                history_insert(name,embed_links)
+            return render_template("rec.html",recommendation=top_tracks,names=artist_name)
         else:
-            return "No artist found"
+            return render_template("error.html",message="Artist not found")
 
 @app.route('/track')
 def track(name):
@@ -48,15 +52,17 @@ def track(name):
         if seed_track['tracks']['items']:
             track_uri = seed_track['tracks']['items'][0]['uri']
         else:
-            return render_template("error.html")
+            return render_template("error.html",message="Track not found")
         print(track_uri)
         recommendations = sp.recommendations(seed_tracks=[track_uri], limit=8)
         embed_links = [f"{track['id']}" for track in recommendations['tracks']]
         print(recommendations)
         if embed_links:
+            if user_active:
+                history_insert(name,embed_links)
             return render_template("rec.html",recommendation=recommendations,urls=embed_links,names=track_name)
         else:
-            return "Track not found 404"
+            return render_template("error.html",message="No similar tracks found")
 
 @app.route('/search', methods=["GET", "POST"])
 def search():
@@ -67,34 +73,272 @@ def search():
             return artist(name) 
         elif options == "track":
             return track(name) 
+        elif options == "genre":
+            return genre(name)
         else:
             return render_template("ioption.html")
     else:
         return "Method not allowed" 
 
-
-
 @app.route('/genre', methods =["GET", "POST"])
-def genre():
-    if request.method=="POST":
-        genre_name=request.form["aname"]
-        recommendations=sp.recommendations(seed_genres=genre_name, limit=12)
+def genre(name):
+        genre_name=name
+        recommendations=sp.recommendations(seed_genres=[genre_name], limit=8)
         print(recommendations)
         if recommendations['tracks']:
             embed_links = [f"https://open.spotify.com/embed/track/{track['id']}" for track in recommendations['tracks']]
             print(f"Embed link: {embed_links}")
             if embed_links:
+                if user_active:
+                    history_insert(name,embed_links)
                 return render_template("rec.html", recommendation=recommendations, urls=embed_links,names=genre_name)
             else:
-                return "No embed links found."
+                return render_template("error.html",message="No similar tracks found")
         else:
-            return "No recommendations found for the selected genre."
+            return render_template("error.html",message="Genre not found")
+        
+        
+def history_insert(name,embed_links):
+    db.execute("USE spotify")
+    result=','.join(embed_links)
+    query="INSERT INTO history (email,search,suggested) VALUES (%s,%s,%s)"
+    data=(email_id,name,result)
+    db.execute(query,data)
+    myconn.commit()
+        
+@app.route('/creds')
+def creds():
+    if user_active:
+        return render_template("user.html")
     else:
-        return "GO BACK AND SEARCH IN SEARCH BAR!"
+        return render_template("LoginPg.html")
+    
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        x=request.form.get("email")
+        y=request.form.get("password")
+        db.execute("USE spotify")
+        query="SELECT name from creds where email=%s and password=%s"
+        data=(x,y)
+        db.execute(query,data)
+        result=db.fetchall()
+        if result:
+            global user_active,email_id
+            user_active=str(result[0][0])
+            email_id=x
+            return redirect(url_for("home"))
+        else:
+            return "failed"
+    else:
+        return "<h1>try another way</h1>"
+
+@app.route('/condition_log')
+def condition_log():
+    return render_template("signup.html")
+
+@app.route('/condition_sign')
+def condition_sign():
+    return render_template("LoginPg.html")
+
+def new_user(a,b,c):
+    db.execute("USE spotify")
+    x=a
+    y=b
+    z=c
+    query="SELECT name from creds where email=%s"
+    data=(x,)
+    db.execute(query,data)
+    result=db.fetchall()
+    if result :
+        return render_template("signup.html")
+    else:
+        insert_query = "INSERT INTO creds ( email,name, password) VALUES ( %s, %s, %s)"
+        data = (x, y, z)
+        db.execute(insert_query, data)
+        myconn.commit()
+        return None
+
+@app.route('/signup',methods=['GET','POST'])
+def signup():
+    if request.method=='POST':
+        x=request.form.get("email")
+        y=request.form.get("name")
+        z=request.form.get("password")
+        new_user(x,y,z)
+        return render_template("LoginPg.html")
+    else:
+        return "<h1>try another way</h1>"
+
+
+@app.route('/user_menu',methods=['GET','POST'])
+def user_menu():
+    if request.method=='POST':
+        x=request.form.get('button')
+        print(x)
+        if x=="history":
+            db.execute("USE spotify")
+            query="SELECT search from history where email=%s"
+            data=(email_id,)
+            db.execute(query,data)
+            result=db.fetchall()
+            print(data)
+            print(result)
+            return render_template("history.html",history=result)
+        else:
+            return f"{x}"
+    else:
+        return "wrong method"
+    
+
+@app.route('/suggested',methods=['GET','POST'])
+def suggested():
+    if request.method=='POST':
+        name=request.form.get('button')
+        db.execute("USE spotify")
+        query="SELECT suggested from history where email=%s and search=%s"
+        data=(email_id,name)
+        db.execute(query,data)
+        result=db.fetchall()
+        print(result)
+        if result:
+            embed_links = result[0][0].split(',')
+            return render_template("history_songs.html",songs=embed_links)
+    else:
+        return "wrong method"
 
 
 if __name__=="__main__":
     app.run(debug=True)
+
+        
+# @app.route('/year', methods =["GET", "POST"])
+# def year(name):
+#         year_release={'from': '2010', 'to': '2020'}
+#         print(type(year_release))
+#         recommendations=sp.recommendations(release_date_range=year_release,limit=12)
+#         print(recommendations)
+#         if recommendations['tracks']:
+#             embed_links = [f"https://open.spotify.com/embed/track/{track['id']}" for track in recommendations['tracks']]
+#             print(f"Embed link: {embed_links}")
+#             if embed_links:
+#                 return render_template("rec.html", recommendation=recommendations, urls=embed_links,names=name)
+#             else:
+#                 return render_template("error.html",message="No tracks found")
+#         else:
+#             return render_template("error.html",message="Date issue")
+
+# @app.route('/year', methods =["GET", "POST"])
+# def year(name):
+#         year_release=name
+#         print(type(year_release))
+#         recommendations=sp.recommendations(target_tempo=year_release)
+#         print(recommendations)
+#         if recommendations['tracks']:
+#             embed_links = [f"https://open.spotify.com/embed/track/{track['id']}" for track in recommendations['tracks']]
+#             print(f"Embed link: {embed_links}")
+#             if embed_links:
+#                 return render_template("rec.html", recommendation=recommendations, urls=embed_links,names=name)
+#             else:
+#                 return render_template("error.html",message="No tracks found")
+#         else:
+#             return render_template("error.html",message="Date issue")
+
+
+
+
+
+
+
+
+
+
+# from matplotlib import artist
+# import spotipy
+# from flask import Flask,render_template,request
+
+# # Create a Spotipy object
+# sp = spotipy.Spotify(auth_manager=spotipy.oauth2.SpotifyOAuth(client_id='a24c797e8e9a487a8c6f9aaceadd3eab', client_secret='87183cc80b114379b637810a397c3f60', redirect_uri='http://127.0.0.1:5000/'))
+
+# # Get the user's access token
+# access_token = sp.auth_manager.get_access_token()['access_token']
+
+
+# app=Flask(__name__)
+
+# @app.route('/')
+# def home():
+#    return render_template("songrec.html")
+
+
+# @app.route('/artist', methods =["GET", "POST"])
+# def artist():
+#     if request.method == "POST":
+#         artist_name=request.form["aname"]
+#         find_artist = sp.search(q=f'artist:"{artist_name}"', type='artist',limit=1)
+
+#         if find_artist['artists']['items']:
+#             artist_uri = find_artist['artists']['items'][0]['uri']
+#         else:
+#             print(f"No artist found for '{artist_name}'")
+#         if artist_uri:
+#             top_tracks = sp.artist_top_tracks(artist_uri)
+#             artist_tracks = top_tracks['tracks']
+#             embed_links = [f"{track['id']}" for track in artist_tracks]
+#             print(f"Embed link: {embed_links}")
+
+#             return render_template("rec.html",recommendation=top_tracks,urls=embed_links)
+#         else:
+#             return "No artist found"
+#     else:
+#         return "GO BACK AND SEARCH IN SEARCH BAR!"
+
+# @app.route('/track', methods =["GET", "POST"])
+# def track():
+#     if request.method=="POST":
+#         track_name=request.form["aname"]
+#         seed_track=sp.search(q=track_name,type="track",limit=1)
+#         if seed_track['tracks']['items']:
+#             track_uri = seed_track['tracks']['items'][0]['uri']
+#         else:
+#             print( "Invalid Track Name")
+#         print(track_uri)
+#         recommendation = sp.recommendations(seed_tracks=[track_uri], limit=8)
+#         embed_links = [f"{track['id']}" for track in recommendation['tracks']]
+#         print(f"Embed link: {embed_links}")
+#         if embed_links:
+#             return render_template("rec.html",recommendation=recommendation,urls=embed_links)
+#         else:
+#             return "Track not found 404"
+#     else:
+#         return "GO BACK AND SEARCH IN SEARCH BAR!"
+
+
+# if __name__=="__main__":
+#     app.run(debug=True)
+
+# @app.route('/genre', methods =["GET", "POST"])
+# def genre():
+#     if request.method=="POST":
+#         genre_name=request.form["aname"]
+#         recommendations=sp.recommendations(seed_genres=genre_name, limit=12)
+#         print(recommendations)
+#         if recommendations['tracks']:
+#             embed_links = [f"https://open.spotify.com/embed/track/{track['id']}" for track in recommendations['tracks']]
+#             print(f"Embed link: {embed_links}")
+#             if embed_links:
+#                 return render_template("rec.html", recommendation=recommendations, urls=embed_links)
+#             else:
+#                 return "No embed links found."
+#         else:
+#             return "No recommendations found for the selected genre."
+#     else:
+#         return "GO BACK AND SEARCH IN SEARCH BAR!"
+
+
+
+
 
 
 # Handle the case when the method is not POST (e.g., when the user accesses the page)
@@ -145,6 +389,15 @@ if __name__=="__main__":
 #     seed_track=sp.search(q=track_name,type="track",limit=5)
 #     if seed_track['track']['']
 #     recommendations = sp.recommendations(seed_tracks=seed_tracks, limit=1)
+
+
+
+
+
+
+
+
+
 
 # @app.route('/')
 # def home():
